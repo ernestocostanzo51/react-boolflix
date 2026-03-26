f0168871736440958b3c61443c66ec2665407aee
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-26 18:07:29 +0100

    creazione card per film e serie

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import "/node_modules/flag-icons/css/flag-icons.min.css";
-import AppHeader from "./components/appHeader";
+
 function App() {
   const api_key = import.meta.env.VITE_API_KEY
   
-
+  const [ricerca, setRicerca] = useState("")
   const [film, setFilm] = useState([])
   const [serie, setSerie] = useState([])
-  const [ricerca, setRicerca] = useState("")
+  
 
 
 useEffect(() => {
@@ -23,21 +23,53 @@ useEffect(() => {
     .then(data => setSerie(data.results)); 
 }, []);
 
+    function Ricerca(e){
+      
 
+e.preventDefault()
+
+//RICERCA FILM
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${ricerca.trim()}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setFilm(data.results);
+      })
+//RICERCA SERIE
+      fetch(`https://api.themoviedb.org/3/search/tv?api_key=${api_key}&query=${ricerca.trim()}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSerie(data.results);
+      })
+    }
 //film e serie caricati con la ricerca(dopo l'invio del form)
 
   
   return (
     <>
   
+     <header>
+<div className="container">
+      <div className="row">
+        <div className="col">
+       <form onSubmit={Ricerca}>
+      <input type="text" value={ricerca} onChange={(e => setRicerca(e.target.value))}></input>
+      <button>Ricerca</button>
+    </form>
+        </div>
+      </div>
+    </div>
+  </header>
     
-    <AppHeader/>
-    <div>
-      <h1>FILM</h1>
-      {
+    
+     
+      <div className="container">
+         <h1>FILM</h1>
+        <div className="row">
+         {
         film.map((item) =>(
-          <div key={item.id} className="card">
-            <img src={`https://image.tmdb.org/t/p/w185${item.poster_path}`}/>
+          <div className="col-2"key={item.id}>
+          <div  className="card">
+            <img src={`https://image.tmdb.org/t/p/w342${item.poster_path}`}/>
             <p>Titolo: {item.title}</p>
             <p>Titolo Reale:{item.original_title}</p>
             <p>Lingua:<span className={`fi fi-${item.original_language === 'en' ? 'us' : item.original_language}`}></span>
@@ -53,35 +85,42 @@ useEffect(() => {
               {(item.vote_average/2).toFixed(1) > 3 && <span style={{ color: "gold" }}>★</span>}
               {(item.vote_average/2).toFixed(1) > 4 && <span style={{ color: "gold" }}>★</span>}
               </p>
-          </div>
-        ))
+          
+    </div>
+ </div>
+        ))}
+</div>
+</div>
 
-
-      }
-      <h1>SERIE</h1>
-      {
-        serie.map((item) =>(
-          <div key={item.id} className="card">
-             <img src={`https://image.tmdb.org/t/p/w185${item.poster_path}`}/>
+           <div className="container">
+            <h1>SERIE</h1>
+            <div className="row">
+               {
+    serie.map((item) =>(
+          <div className="col-2"key={item.id}>
+          <div  className="card">
+            <img src={`https://image.tmdb.org/t/p/w342${item.poster_path}`}/>
             <p>Titolo: {item.name}</p>
-             <p>Titolo Reale: {item.original_name}</p>
-            <p>Lingua: <span className={`fi fi-${item.original_language === 'en' ? 'us' : item.original_language}`}> </span>
+            <p>Titolo Reale:{item.original_name}</p>
+            <p>Lingua:<span className={`fi fi-${item.original_language === 'en' ? 'us' : item.original_language}`}></span>
             <span style={{ marginLeft: '10px' }}>({item.original_language})</span>
-            </p>
-            <p>Voto: {(item.vote_average/2).toFixed(1)}
-              
-                {item.vote_average === 0 && <span> NO VALUE </span>}
+           </p>
+            <p>{(item.vote_average/2).toFixed(1)}
+
+              {item.vote_average === 0 && <span> NO VALUE </span>}
               
               {(item.vote_average/2).toFixed(1) > 0 && <span style={{ color: "gold" }}>★</span>}
               {(item.vote_average/2).toFixed(1) > 1 && <span style={{ color: "gold" }}>★</span>}
               {(item.vote_average/2).toFixed(1) > 2 && <span style={{ color: "gold" }}>★</span>}
               {(item.vote_average/2).toFixed(1) > 3 && <span style={{ color: "gold" }}>★</span>}
               {(item.vote_average/2).toFixed(1) > 4 && <span style={{ color: "gold" }}>★</span>}
-            </p>
-          </div>
-        ))
-      }
+              </p>
+          
     </div>
+ </div>
+        ))}
+            </div>
+           </div>
     </>
   )
 }
