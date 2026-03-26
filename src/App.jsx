5abd8bf30eb812b6a9ff40a432d1d39793ec448f
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-26 11:30:18 +0100

    chiave env

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 
 
 function App() {
-  const link_api = `https://api.themoviedb.org/3/search/movie?api_key={}d&query=ritorno+al+fut
+  const api_key = import.meta.env.VITE_API_KEY
+  const link_api = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}d&query=ritorno+al+fut
 uro`
-import.meta.VITE_API_KEY
+
+console.log(import.meta.env.VITE_API_KEY);
 
 
   return (
