841c6b8a3777bf3a9f5ec5134de33db640f028f1
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-26 11:20:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  const link_api = `https://api.themoviedb.org/3/search/movie?api_key={}d&query=ritorno+al+fut
+uro`
+import.meta.VITE_API_KEY
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
