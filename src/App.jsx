da46e322df1bf7d3db3f96fbd6e1d706811d565b
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-26 12:19:54 +0100

    creazione form e chiamata API

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
   const api_key = import.meta.env.VITE_API_KEY
-  const link_api = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}d&query=ritorno+al+fut
-uro`
-
+  const link_api = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}d&query=ritorno+al+futuro`
+  const [film, setFilm] = useState([])
 console.log(import.meta.env.VITE_API_KEY);
+
+useEffect(() =>{
+  fetch(link_api)
+  .then(res => res.json())
+  .then(data => {
+    setFilm(data.results)
+  })
+})
+
+console.log(film)
 
 
 
   return (
     <>
-    
+    <form>
+      <input type="text"></input>
+      <button>Ricerca</button>
+    </form>
+
+
+    <div>
+      {
+
+
+      }
+    </div>
     </>
   )
 }
